b2aa9c6550966c750cc3cd31dcd7129401f77cbf
de.bieber <de.bieber@8bd5a5ea-0263-11de-802b-177316e4ac06>
2009-03-25 18:22:53 +0000

    ALSO: model.Domain: Mahnliste ist einem Ausleiher zugeordnet; die lädt alle Exemplare für den Ausleiher. model.logic: Mahnlisteverwalter: Erstellt Mahnlisten (Struktur: Map); fragt vor direkt das exemplar DAO nach allen exemplaren, die fehlen.

--- trunk/src/bibliotheksverwaltung/model/domain/Mahnliste.java
+++ trunk/src/bibliotheksverwaltung/model/domain/Mahnliste.java
@@ -1,9 +1,59 @@
-/**
- * Bieber, Dennis
- */
 package bibliotheksverwaltung.model.domain;
 
-public class Mahnliste {
-	
+import java.util.List;
 
+import bibliotheksverwaltung.model.daos.dao.MySQLAusleiherDAO;
+import bibliotheksverwaltung.model.daos.dao.MySQLExemplarDAO;
+
+/**
+ * @author dBi
+ */
+public class Mahnliste {
+
+	/**
+	 * Jede Mahnliste ist einem Ausleiher zugeordnet.
+	 */
+	private Ausleiher ausleiher;
+
+	/**
+	 * Die Liste der zurückgehaltenen Exemplare.
+	 */
+	private List<Exemplar> fehlListe;
+
+	/**
+	 * 
+	 * @param ausleiherID
+	 */
+	public Mahnliste(int ausleiherID) {
+		ausleiher = new MySQLAusleiherDAO().get(ausleiherID);
+		fehlListe = new MySQLExemplarDAO().fehlt(ausleiher.getId());
+	}
+
+	/**
+	 * 
+	 * @param a
+	 */
+	public Mahnliste(Ausleiher a) {
+		ausleiher = a;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Für Ausleiher mit der ID ");
+		sb.append(ausleiher.getId());
+		sb.append(" fehlen folgende Exemplare: ");
+		for (Exemplar e : fehlListe) {
+			sb.append(' ');
+			sb.append(e.getMedium());
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Mahnliste(1));
+	}
 }
